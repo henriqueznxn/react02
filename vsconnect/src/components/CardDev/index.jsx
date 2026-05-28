a7afe85bed99b3f3d65e2035f8b9ae93c9a74b5c
Henrique De Sousa Lima <sousahenrique.sep@gmail.com>
2026-05-27 21:50:29 -0300

    njkasn

---
+++ vsconnect/src/components/CardDev/index.jsx
@@ -0,0 +1,35 @@
+import "./style.css";
+
+import { Link } from "react-router-dom";
+
+function Card(props) {
+    
+    function verificarLista() {
+        if (typeof props.listaTeachs === "string") {
+        
+            return JSON.parse(props.listaTeachs)
+
+        } else { 
+        return props.listaTeachs;
+        }
+    }
+    return (
+        <div className="card_dev">
+
+            <div className="grupo_contato">
+                <img src={"http://localhost:3000/static/" + props.foto} alt=""/>
+
+                <div className="contato_dev">
+                    <Link to={"/perfil/" + props.id}>
+                    <h2>{props.nome}</h2>
+                    </Link>
+                    <p>{props.email}</p>
+                </div>
+            </div>
+
+            <div className="techs">
+               {verificarListaTechs()}
+            </div>
+        </div>
+    )
+}
